7104553590ab2c04d1f04f2b33f2cde341468ab2
Tek Jones <tekthree@gmail.com>
2023-03-07 13:08:05 -0800

    adds card styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
+    colors: {
+      mcBg: '#13141c',
+      cardColor: '#212330',
+      mt: '#898a91',
+      mtLight: '#adaeb7',
+    },
   },
   plugins: [require('@tailwindcss/line-clamp')],
 };
